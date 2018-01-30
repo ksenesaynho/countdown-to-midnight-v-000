fdbfecbd20238546480ed2363c9610681692ce5f
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-01-30 20:27:38 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
-#write your code here
-
-def countdown(number)
+def countdown(seconds_to_midnight)]
+  while seconds_to_midnight > log10puts "#{seconds_to_midnight} SECOND(S)!"
+  seconds_to_midnight -= 1
 end
+"HAPPY NEW YEAR"
+end
+
+def
