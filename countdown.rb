524ee24313ae51bee74ad40c3ce2746fc66442cc
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-01-30 20:29:42 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,9 @@
-def countdown(seconds_to_midnight)]
-  while seconds_to_midnight > log10puts "#{seconds_to_midnight} SECOND(S)!"
-  seconds_to_midnight -= 1
-end
-"HAPPY NEW YEAR"
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(seconds_to_midnight)
